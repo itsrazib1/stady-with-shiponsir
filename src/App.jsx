02402479f8fca990695adbb2be452878f5f95e9f
itsrazib1 <itsrazib1@gmail.com>
2023-12-04 15:54:08 +0600

    added

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+
+
+const App = () => {
+  return (
+
+    <div>
+     
+      <div className="mockup-browser border bg-base-300">
+  <div className="mockup-browser-toolbar">
+    <div className="input">https://itsrazib.fun</div>
+  </div>
+  <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+</div>
+<div className="tabs tabs-lifted py-2">
+  <a className="tab">Messages</a> 
+  <a className="indicator tab tab-active">
+    Notifications 
+    <span className="indicator-item badge">8</span>
+  </a> 
+  <a className="tab">Requests</a>
+</div>
+      <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+<div className="sm:flex flex-none sm:px-20 px-10 py-5 gap-3">
+<div className="mockup-phone border-primary">
+  <div className="camera"></div> 
+  <div className="display">
+    <div className="artboard artboard-demo phone-1">RaziB</div>
+  </div>
+</div>
+<div className="mockup-phone">
+  <div className="camera"></div> 
+  <div className="display">
+    <div className="artboard artboard-demo phone-1">ShraboN</div>
+  </div>
+</div>
+<div className="mockup-phone border-primary">
+  <div className="camera"></div> 
+  <div className="display">
+    <div className="artboard artboard-demo phone-1">Ohi</div>
+  </div>
+</div>
+<div className="mockup-phone">
+  <div className="camera"></div> 
+  <div className="display">
+    <div className="artboard artboard-demo phone-1">Rezual</div>
+  </div>
+</div>
+</div>
+
+    </div>
+  );
+};
+
+export default App;
