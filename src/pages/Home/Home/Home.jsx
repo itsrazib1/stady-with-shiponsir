90e534c276f8b9815a2298596022eef8fd0ad975
rezaul118255 <rezaul118255@gmail.com>
2023-12-06 11:11:47 +0600

    navbar compleate

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import PartOne from "../PartOne/PartOne";
 import PartTwo from "../PartTwo/PartTwo";
 
 
 const Home = () => {
     return (
         <div>
+            <PartOne></PartOne>
             <PartTwo></PartTwo>
         </div>
     );
