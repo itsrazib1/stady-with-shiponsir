0d9a8adaed1c49f5b861b8083a5bfe3fa027a25f
AB-ohi <askohi63907@gmail.com>
2023-12-13 08:11:52 +0600

    added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import PartFour from "../PartFour/PartFour";
-=======
 import PartOne from "../PartOne/PartOne";
->>>>>>> 407d6db8c8a78dc99baa98bd6536864f1bb1110d
 import PartTwo from "../PartTwo/PartTwo";
 import Partfive from "../Partfive/Partfive";
 
